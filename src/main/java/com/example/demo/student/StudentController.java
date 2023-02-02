d4445c825954bd9f8168d6878a1f7e99db8f00f6
Damith <37-se-0005@kdu.ac.lk>
2023-02-02 22:21:02 +0530

    Add post and delete requests

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -2,7 +2,11 @@ package com.example.demo.student;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +22,16 @@ public class StudentController {
     @GetMapping
 	public List<Student> fetchAllStudents() {
 		return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student){
+            studentService.addNewStudent(student);
+        }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(
+        @PathVariable("studentId") String studentId) {
+        studentService.deleteStudent(studentId);
     }
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -18,4 +18,20 @@ public class StudentService {
     public List<Student> getStudents() {
 		    return studentRepository.findAll();
     }
+
+    public void addNewStudent(Student student){
+        System.out.println("Added new student");
+    }
+
+
+    public void deleteStudent(String studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        
+        if(!exists){
+            throw new IllegalStateException(
+                "Student with id " + studentId + " does not exist"
+            );
+        }
+        studentRepository.deleteById(studentId);
+    }
 }
